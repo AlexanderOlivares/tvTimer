d83e8da72c394636fb206056263cc99f8861cac2
alex olivares <alexander.b.olivares@gmail.com>
2021-04-21 21:32:07 -0500

    delay working

--- main.js
+++ main.js
@@ -23,7 +23,31 @@ let saveSec = document.getElementById("saveSec");
 let saveDelay = document.getElementById("saveDelay");
 let saveTimer = document.getElementById("saveTimer");
 
-function timer() {
+const timesToSpeak = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 30];
+
+let isMuted = false;
+let onDelay = true;
+
+let delayLength;
+let timerInterval;
+
+start.addEventListener("click", () => {
+  delayLength = +delay.value;
+  startTimer(delayLength);
+  hideInputs();
+});
+
+const startTimer = delayLength => {
+  timerInterval = setInterval(() => {
+    timer(delayLength);
+    start.disabled = true;
+    displayHour.innerText = addLeadingZero(hour.value);
+    displayMin.innerText = addLeadingZero(min.value);
+    displaySec.innerText = addLeadingZero(sec.value);
+  }, 1000);
+};
+
+const timer = delayLength => {
   if (hour.value == 0 && min.value == 0 && sec.value == 0) {
     hour.value = 00;
     min.value = 00;
@@ -35,39 +59,101 @@ function timer() {
     min.value--;
   } else if (hour.value != 0 && min.value == 0) {
     hour.value--;
-    min.value = 60;
+    min.value = 59;
     sec.value = 59;
   } else {
     alert("An error occured. Please refresh the browser.");
-    return;
   }
-}
 
-function addLeadingZero(strOfNum) {
+  if (hour.value == 0 && min.value == 0 && sec.value <= 10) {
+    displaySec.style.backgroundColor = "red";
+  }
+
+  if (!isMuted) {
+    if (onDelay) {
+      voiceCountdownOnDelay(delayLength);
+    } else {
+      voiceCountdown();
+    }
+  }
+};
+
+const voiceCountdownOnDelay = delayLength => {
+  speakSecondsLeft(hour.value, min.value, `${sec.value - delayLength}`);
+  speakMinutesLeft(hour.value, min.value, `${sec.value - delayLength}`);
+  timeExpired(hour.value, min.value, sec.value);
+};
+
+const voiceCountdown = () => {
+  speakSecondsLeft(hour.value, min.value, sec.value);
+  speakMinutesLeft(hour.value, min.value, sec.value);
+  timeExpired(hour.value, min.value, sec.value);
+};
+
+const speakSecondsLeft = (hour, min, sec) => {
+  if (+hour > 0) return;
+  if (+min > 0) return;
+
+  const secondsInNumbers = Number(sec);
+
+  if (timesToSpeak.includes(secondsInNumbers)) {
+    responsiveVoice.speak(sec);
+  }
+};
+
+const speakMinutesLeft = (hour, min, sec) => {
+  if (+hour > 0) return;
+
+  if (+min === 2 && +sec === 0) {
+    responsiveVoice.speak("two minutes");
+  }
+
+  if (+min === 1 && +sec === 30) {
+    responsiveVoice.speak("one minute and thirty seconds");
+  }
+
+  if (+min === 1 && +sec === 0) {
+    responsiveVoice.speak("one minute");
+  }
+};
+
+const timeExpired = (hour, min, sec) => {
+  if (+hour === 0 && +min === 0 && +sec === 0) {
+    responsiveVoice.speak("time expired");
+    clearInterval(timerInterval);
+    showInputs();
+  }
+};
+
+const addLeadingZero = strOfNum => {
+  if (strOfNum === "00") {
+    return "00";
+  }
   return +strOfNum < 10 ? `0${strOfNum}` : strOfNum;
-}
+};
 
-let timerInterval;
-function startTimer() {
-  timerInterval = setInterval(() => {
-    timer();
-    displayHour.innerText = hour.value;
-    displayMin.innerText = addLeadingZero(min.value);
-    displaySec.innerText = addLeadingZero(sec.value);
-  }, 1000);
-}
+reset.addEventListener("click", () => {
+  resetTimer();
+  showInputs();
+});
 
-function resetTimer() {
+const resetTimer = () => {
   clearInterval(timerInterval);
+  start.disabled = false;
   hour.value = 0;
   min.value = 0;
   sec.value = 0;
   displayHour.innerText = "00";
   displayMin.innerText = "00";
   displaySec.innerText = "00";
-}
+};
 
-function hideInputs() {
+mute.addEventListener("click", () => {
+  isMuted = !isMuted;
+  mute.classList.toggle("btn-danger");
+});
+
+const hideInputs = () => {
   inputHour.style.display = "none";
   inputMin.style.display = "none";
   inputSec.style.display = "none";
@@ -75,9 +161,9 @@ function hideInputs() {
   delayHeader.style.display = "none";
   delay.style.display = "none";
   setTimer.style.display = "none";
-}
+};
 
-function showInputs() {
+const showInputs = () => {
   inputHour.style.display = "block";
   inputMin.style.display = "block";
   inputSec.style.display = "block";
@@ -85,13 +171,4 @@ function showInputs() {
   delayHeader.style.display = "block";
   delay.style.display = "block";
   setTimer.style.display = "block";
-}
-start.addEventListener("click", () => {
-  startTimer();
-  hideInputs();
-});
-
-reset.addEventListener("click", () => {
-  resetTimer();
-  showInputs();
-});
+};
